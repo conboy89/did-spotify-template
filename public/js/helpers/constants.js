9b624a7f4f13c5c44b91dbea0d3ac7831d55e48e
conboy89 <38505513+conboy89@users.noreply.github.com>
2018-11-26 20:28:06 +0000

    Add files via upload
    manually uploaded files as can't get command line to work even after watching videos

--- public/js/helpers/constants.js
+++ public/js/helpers/constants.js
@@ -1,3 +1,3 @@
 export const BASE_URL = 'http://localhost:8888/';
-export const CLIENT_ID = '';
+export const CLIENT_ID = '90307d01ec1d41f8aa1caef8c91b2c25';
 export const STATE_KEY = 'spotify_auth_state';
